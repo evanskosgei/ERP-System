a9342f4ed5e3b745c08eb7b18722c75409512fd6
ek-codefremics <ekosgei@codefremics.com>
2024-04-29 10:38:54 +0300

    api`s

--- src/api/mtaApi.ts
+++ src/api/mtaApi.ts
@@ -7,7 +7,17 @@ const mtaApi = {
   },
  supplier: {
   addSupplier: (data: any) => post("/supplier", data),
-  getSuppliers: get("/supplier")
+  createSupplierContact: (id: any, data: any) => post(`/supplier/contact/${id}`, data),
+  getSuppliers: get("/supplier"),
+  getASupplier: (id: any) => get(`/supplier/${id}`),
+  getSupplierContact: (id: any,) => get(`/supplier/contact/${id}`),
+  getSpecificSupplierContacts: (id: any, contact_id:any) => get(`/supplier/contact/${id}/${contact_id}`),
+
+  deleteSupplier: (id: any) => del(`/supplier/${id}`),
+  deleteSupplierContact: (id: any, contact_id: any) => del(`/supplier/contact/${id}/${contact_id}`),
+
+  // updateSupplier: (id: any, data: any) => put(`/supplier/${id}`, data),
+  // updateSupplierContact: (id: any, contact_id: any, data: any) => put(`/supplier/contact/${id}/${contact_id}`, data),
  },
 };
 
