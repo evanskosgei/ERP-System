3c9c1ec7f7e083df49265c60bfab41a626619eb4
arnold-cf <aalukwe@codefremics.com>
2024-05-22 10:13:32 +0300

    Updated create user functio

--- src/views/general/main_dashboard/main_dashboard.jsx
+++ src/views/general/main_dashboard/main_dashboard.jsx
@@ -20,15 +20,15 @@ const MainDashboard = ({local_varaiable}) => {
 											<path className="fill-primary" d="M9,10h2.5c0.276123,0,0.5-0.223877,0.5-0.5S11.776123,9,11.5,9H10V8c0-0.276123-0.223877-0.5-0.5-0.5S9,7.723877,9,8v1c-1.1045532,0-2,0.8954468-2,2s0.8954468,2,2,2h1c0.5523071,0,1,0.4476929,1,1s-0.4476929,1-1,1H7.5C7.223877,15,7,15.223877,7,15.5S7.223877,16,7.5,16H9v1.0005493C9.0001831,17.2765503,9.223999,17.5001831,9.5,17.5h0.0006104C9.7765503,17.4998169,10.0001831,17.276001,10,17v-1c1.1045532,0,2-0.8954468,2-2s-0.8954468-2-2-2H9c-0.5523071,0-1-0.4476929-1-1S8.4476929,10,9,10z M21.5,12H17V2.5c0.000061-0.0875244-0.0228882-0.1735229-0.0665283-0.2493896c-0.1375732-0.2393188-0.4431152-0.3217773-0.6824951-0.1842041l-3.2460327,1.8603516L9.7481079,2.0654297c-0.1536865-0.0878906-0.3424072-0.0878906-0.4960938,0l-3.256897,1.8613281L2.7490234,2.0664062C2.6731567,2.0227661,2.5871582,1.9998779,2.4996338,1.9998779C2.2235718,2.000061,1.9998779,2.223938,2,2.5v17c0.0012817,1.380188,1.119812,2.4987183,2.5,2.5H19c1.6561279-0.0018311,2.9981689-1.3438721,3-3v-6.5006104C21.9998169,12.2234497,21.776001,11.9998169,21.5,12z M4.5,21c-0.828064-0.0009155-1.4990845-0.671936-1.5-1.5V3.3623047l2.7412109,1.5712891c0.1575928,0.0872192,0.348877,0.0875854,0.5068359,0.0009766L9.5,3.0761719l3.2519531,1.8583984c0.157959,0.0866089,0.3492432,0.0862427,0.5068359-0.0009766L16,3.3623047V19c0.0008545,0.7719116,0.3010864,1.4684448,0.7803345,2H4.5z M21,19c0,1.1045532-0.8954468,2-2,2s-2-0.8954468-2-2v-6h4V19z"></path>
 										</svg>
 									</div>
-									<h6 className="text-lg font-medium text-gray-800 mb-2 dark:text-white my-auto">Total Revenue </h6>
+									<h6 className="text-lg font-medium text-gray-800 mb-2 dark:text-white my-auto">Total Sales </h6>
 								</div>
 								{/* <span className="badge bg-primary/10 text-primary py-1 ltr:ml-auto rtl:mr-auto !my-auto">
 									<i className="ti ti-trending-up"></i> 20%
 								</span> */}
 							</div>
 							<div className="mt-2">
-								<h2 className="text-2xl font-semibold text-gray-800 dark:text-white">Ksh 26,35,262</h2>
-								<p className="text-xs text-gray-400 ">in last week</p>
+								<h2 className="text-2xl font-semibold text-gray-800 dark:text-white">Ksh 1,900,506</h2>
+								<p className="text-xs text-gray-400 ">In this week</p>
 							</div>
 						</div>
 					</div>
@@ -46,14 +46,14 @@ const MainDashboard = ({local_varaiable}) => {
 											</path>
 										</svg>
 									</div>
-									<h6 className="text-lg font-medium text-gray-800 mb-2 dark:text-white my-auto">Total Sales</h6>
+									<h6 className="text-lg font-medium text-gray-800 mb-2 dark:text-white my-auto">Monthly Sales Target</h6>
 								</div>
 								{/* <span className="badge bg-secondary/10 text-secondary py-1 ltr:ml-auto rtl:mr-auto !my-auto"><i
 									className="ti ti-trending-up"></i> 1.8%</span> */}
 							</div>
 							<div className="mt-2">
-								<h2 className="text-2xl font-semibold text-gray-800 dark:text-white">Ksh 56,35,262</h2>
-								<p className="text-xs text-gray-400 ">in last week</p>
+								<h2 className="text-2xl font-semibold text-gray-800 dark:text-white">Ksh 2,500,000</h2>
+								<p className="text-xs text-gray-400 ">Target this month</p>
 							</div>
 						</div>
 					</div>
@@ -71,14 +71,14 @@ const MainDashboard = ({local_varaiable}) => {
 											</path>
 										</svg>
 									</div>
-									<h6 className="text-lg font-medium text-gray-800 mb-2 dark:text-white my-auto">Total Products</h6>
+									<h6 className="text-lg font-medium text-gray-800 mb-2 dark:text-white my-auto">Stock Levels</h6>
 								</div>
 								{/* <span className="badge bg-warning/10 text-warning py-1 ltr:ml-auto rtl:mr-auto !my-auto"><i
 									className="ti ti-trending-down"></i> 1.8%</span> */}
 							</div>
 							<div className="mt-2">
-								<h2 className="text-2xl font-semibold text-gray-800 dark:text-white">Ksh 4,262</h2>
-								<p className="text-xs text-gray-400 ">in last week</p>
+								<h2 className="text-2xl font-semibold text-gray-800 dark:text-white">Ksh 34,262,458</h2>
+								<p className="text-xs text-gray-400 ">Total Stock This Week</p>
 							</div>
 						</div>
 					</div>
@@ -114,8 +114,6 @@ const MainDashboard = ({local_varaiable}) => {
 
 			<div className="grid grid-cols-12 gap-x-5">
 			
-				
-
 				<div className="col-span-12 lg:col-span-6 xxl:col-span-4">
 					<div className="box">
 						<div className="box-header flex justify-between">
@@ -140,7 +138,7 @@ const MainDashboard = ({local_varaiable}) => {
 											</span>
 										</div>
 										<div className="crm-timeline-content">
-											<span>Neon Tarly added <span className="text-info font-semibold">Robert Bright</span> to AI summit project.</span>
+											<span>Stock in transit. <span className="text-info font-semibold">4,568</span> items to receive.</span>
 										</div>
 										<div className="flex-auto text-end">
 											<span className="block text-gray-500 dark:text-white/70 text-xs opacity-70">12 hrs</span>
@@ -155,7 +153,7 @@ const MainDashboard = ({local_varaiable}) => {
 											</span>
 										</div>
 										<div className="crm-timeline-content">
-											<span>Replied to new support request <i className="ri-checkbox-circle-line text-success fs-16 align-middle"></i></span>
+											<span>Customer inquiry on after sale <i className="ri-checkbox-circle-line text-success fs-16 align-middle"></i></span>
 										</div>
 										<div className="flex-auto text-end">
 											<span className="block text-gray-500 dark:text-white/70 text-xs opacity-70">4 hrs</span>
@@ -170,7 +168,7 @@ const MainDashboard = ({local_varaiable}) => {
 											</span>
 										</div>
 										<div className="crm-timeline-content">
-											<span className="font-semibold">Update of calendar events &amp;</span><span><Link to="#" className="text-primary font-semibold"> Added new events in next week.</Link></span>
+											<span className="font-semibold">Approve products received &amp;</span><span><Link to="#" className="text-primary font-semibold"> Disbatch products to Managers.</Link></span>
 										</div>
 										<div className="flex-auto text-end">
 											<span className="block text-gray-500 dark:text-white/70 text-xs opacity-70">4:45PM</span>
@@ -185,8 +183,8 @@ const MainDashboard = ({local_varaiable}) => {
 											</span>
 										</div>
 										<div className="crm-timeline-content">
-											<span>New theme for <span className="font-semibold">Spruko Website</span> completed</span>
-											<span className="block fs-12 text-muted">Lorem ipsum, dolor sit amet.</span>
+											<span>Distribution Centers out of Stock <span className="font-semibold">View List</span></span>
+											<span className="block fs-12 text-muted">Products on demand our of stock!.</span>
 										</div>
 										<div className="flex-auto text-end">
 											<span className="block text-gray-500 dark:text-white/70 text-xs opacity-70">3 hrs</span>
@@ -201,7 +199,7 @@ const MainDashboard = ({local_varaiable}) => {
 											</span>
 										</div>
 										<div className="crm-timeline-content">
-											<span>Created a <span className="text-success font-semibold">New Task</span> today <span className="avatar w-5 h-5 bg-purple-transparent rounded-full inline-flex ms-1"><i className="ri-add-fill text-purple fs-12"></i></span></span>
+											<span><span className="text-danger font-semibold">Product Alert!</span> Stock damaged. View Report <span className="avatar w-5 h-5 bg-purple-transparent rounded-full inline-flex ms-1"><i className="ri-add-fill text-purple fs-12"></i></span></span>
 										</div>
 										<div className="flex-auto text-end">
 											<span className="block text-gray-500 dark:text-white/70 text-xs opacity-70">22 hrs</span>
